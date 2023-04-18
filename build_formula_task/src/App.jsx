cd8893dcec42edcad8b495d7a00af0add572a5c7
Shiv-Raj-Singh <mt932747@gmail.com>
2023-04-18 09:54:01 +0530

    to do task done

--- build_formula_task/src/App.jsx
+++ build_formula_task/src/App.jsx
@@ -10,32 +10,11 @@ import TodoPage from "./Pages/Todo";
 export const myContext = createContext()
 
 const reducer = (state , action)=>{
-    console.log(state);
-    // localStorage.setItem('todos' ,  [localStorage.getItem('todos')] ,JSON.stringify(state))
     switch(action.type){
         case 'UPDATE':
-          console.log(state);
            return {
              ...state , [action.field] : action.payload ,
            }
-           case "ADD_TODO":
-            
-            return JSON.parse(localStorage.getItem('todos')) ? { todos: [...state.todos, action.payload , JSON.parse(localStorage.getItem('todos')) ] } : {todos: [...state.todos, action.payload ]}
-          case "REMOVE_TODO":
-            return { todos: state.todos.filter((todo) => todo.id !== action.payload) };
-           
-          case "TOGGLE_TODO":
-          return {
-            todos: state.todos.map((todo) => {
-              if (todo.id === action.payload) {
-                return { ...todo, completed: !todo.completed };
-              }
-              return todo;
-            }),
-          }; 
-        
-          case 'DONE':
-            return action.payload
 
         default:
           return state
